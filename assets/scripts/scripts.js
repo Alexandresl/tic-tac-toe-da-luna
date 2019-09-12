1ece36aab4a777ba4a6af138ccd15f82b7d54384
Alexandre Lima <limaalexguitar@gmail.com>
2019-09-12 00:28:57 -0300

    com controles

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -6,12 +6,50 @@ let computerCharacter;
 let whoPlays = 0; // 0 = jogador 1 = cpu
 let checkTheVictory; // verifica condição de vitória
 let inProgress; // verifica se o jogo está em andamento
-let level = 2; // Guarda o nível de dificuldade
+let gameLevel = 1; // Guarda o nível de dificuldade
 let totalMove; // Contador do números de jogadas da CPU
 let whoStarts = 0; // 0 = jogador 1 = cpu
 let fullLine; // mostra vistoria
 let playEffect = audio = new Audio('./assets/audio/click.wav'); // Audio da jogada
-let effectOnOff = true; // Silencia efeito
+let playEffectHome = audio = new Audio('./assets/audio/click1.wav'); // Audio da jogada
+let playMusic = audio = new Audio('./assets/audio/lunaMusic.mp3'); // Música de fundo
+let audioOnOff = true; // Silencia efeito
+
+// Elementos
+let whoStartsEl = document.getElementById('whoStarts');
+let whoWinEl = document.getElementById('whoWin');
+let whoPlayEl = document.getElementById("whoPlay");
+let selectCharacterEl = document.getElementById('selectCharacter');
+let boardEl = document.getElementById('board');
+let cel1El = document.getElementById('cel1');
+let cel2El = document.getElementById('cel2');
+let cel3El = document.getElementById('cel3');
+let cel4El = document.getElementById('cel4');
+let cel5El = document.getElementById('cel5');
+let cel6El = document.getElementById('cel6');
+let cel7El = document.getElementById('cel7');
+let cel8El = document.getElementById('cel8');
+let cel9El = document.getElementById('cel9');
+let btnAudioEl = document.querySelector('.audioElOn')
+let btnMachineEl = document.querySelector('.machineEl');
+let btnLevelEl = document.querySelector('.levelEl1');
+let btnPlayEl = document.querySelector('.playEl');
+
+function toggleAudio() {
+  audioOnOff = !audioOnOff;
+  if (!audioOnOff) {
+    btnAudioEl.classList.remove('audioElOn');
+    btnAudioEl.classList.add('audioElOff');
+    playMusic.pause();
+  } else {
+    btnAudioEl.classList.remove('audioElOff');
+    btnAudioEl.classList.add('audioElOn');
+    if (playMusic.paused === true) {
+      playMusic.play();
+    }
+    
+  }
+}
 
 function play(position) {
   if (inProgress && whoPlays === 0) {
@@ -73,7 +111,6 @@ function play(position) {
     }
     if (whoPlays === 1) {
       checkTheVictory = verifyVictory();
-
       updateBoard();
       endGame();
       totalMove++;
@@ -84,9 +121,48 @@ function play(position) {
 }
 
 function playEffectFunction() {
-  if (effectOnOff) {
+  if (audioOnOff) {
     playEffect.currentTime = 0;
-    playEffect.play();
+    let playPromise = playEffect.play();
+    if (playPromise !== undefined) {
+      playPromise.then(() => {
+      })
+        .catch(error => {
+          
+        });
+    }
+  }
+}
+
+function level() {
+  switch (gameLevel) {
+    case 1:
+      gameLevel = 2;
+      btnLevelEl.classList.remove('levelEl1');
+      btnLevelEl.classList.add('levelEl2');
+      break;
+    case 2:
+      gameLevel = 3;
+      btnLevelEl.classList.remove('levelEl2');
+      btnLevelEl.classList.add('levelEl3');
+      break;
+    case 3:
+      gameLevel = 1;
+      btnLevelEl.classList.remove('levelEl3');
+      btnLevelEl.classList.add('levelEl1');
+      break;
+  }
+}
+
+function playEffectHomeFunc() {
+  if (audioOnOff) {
+    playEffectHome.currentTime = 0;
+    let playPromise = playEffectHome.play();
+    if (playPromise !== undefined) {
+      playPromise.then(() => {
+      }) .catch(error => {
+      });
+    }
   }
 }
 
@@ -95,13 +171,13 @@ function cpuMoves() {
   if (inProgress === true) {
     setTimeout(() => {
       var i, j;
-      if (level === 1) {
+      if (gameLevel === 1) {
         do {
           i = Math.round(Math.random() * 2);
           j = Math.round(Math.round(Math.random() * 2))
         } while (game[i][j]);
         game[i][j] = computerCharacter;
-      } else if (level === 2) {
+      } else if (gameLevel === 2) {
         // Ataque
         // Linha 1
         if (game[0][0] === 'o' && game[0][1] === 'o' && game[0][2] === '') {
@@ -217,6 +293,8 @@ function cpuMoves() {
             }
           }
         }
+      } else {
+        // Não implementado
       }
 
       checkTheVictory = verifyVictory();
@@ -230,15 +308,15 @@ function cpuMoves() {
 
 }
 
-function updateWhoPlays() {  
+function updateWhoPlays() {
   if (totalMove !== 0) {
-    document.getElementById('whoStarts').style.display = 'none';
-    document.getElementById('whoWin').style.display = 'none';
-    document.getElementById("whoPlay").style.display = 'block';
+    whoStartsEl.style.display = 'none';
+    whoWinEl.style.display = 'none';
+    whoPlayEl.style.display = 'block';
     if (whoPlays === 0 && myCharacter === 'x' || whoPlays === 1 && myCharacter === 'o') {
-      document.getElementById("whoPlay").innerHTML = "Quem joga: Você";
+      whoPlayEl.innerHTML = "Quem joga: Você";
     } else {
-      document.getElementById("whoPlay").innerHTML = "Quem joga: Computador";
+      whoPlayEl.innerHTML = "Quem joga: Computador";
     }
   }
 }
@@ -247,20 +325,20 @@ function endGame() {
   if (checkTheVictory !== '') {
     win = (checkTheVictory === 'x') ? 'Luna' : 'Claudio';
     setTimeout(() => {
-      document.getElementById('whoStarts').style.display = 'none';
-      document.getElementById('whoPlay').style.display = 'none';
-      document.getElementById('whoWin').style.display = 'block';
-      document.getElementById('whoWin').innerHTML = win + ' Ganhou!!!';
+      whoStartsEl.style.display = 'none';
+      whoPlayEl.style.display = 'none';
+      whoWinEl.style.display = 'block';
+      whoWinEl.innerHTML = win + ' Ganhou!!!';
     }, 10);
     inProgress = false;
   }
   if (checkTheVictory === '' && totalMove === 10 && whoStarts === 0 ||
     checkTheVictory === '' && totalMove === 9 && whoStarts === 1) {
     setTimeout(() => {
-      document.getElementById('whoStarts').style.display = 'none';
-      document.getElementById('whoPlay').style.display = 'none';
-      document.getElementById('whoWin').style.display = 'block';
-      document.getElementById('whoWin').innerHTML = 'O jogo empatou!!!';
+      whoStartsEl.style.display = 'none';
+      whoPlayEl.style.display = 'none';
+      whoWinEl.style.display = 'block';
+      whoWinEl.innerHTML = 'O jogo empatou!!!';
     }, 10);
 
   }
@@ -275,8 +353,7 @@ function verifyVictory() {
         board[i][1].children[0].style.backgroundColor = 'white';
         board[i][2].children[0].style.backgroundColor = 'white';
       }
-      console.log('entrou');
-      
+
       return game[i][0];
     }
   }
@@ -332,10 +409,17 @@ function updateBoard() {
 
 }
 
+function activePlay() {
+  btnPlayEl.removeAttribute('disabled');
+  btnPlayEl.classList.remove('disabled');
+}
+
 function start(jogador) {
-  document.getElementById('selectCharacter').style.display = 'none';
-  document.getElementById('board').style.display = 'block';
-  document.getElementById('board').classList.add('animated', 'fadeIn', 'slower');
+  playBackAudio();
+  activePlay();
+  selectCharacterEl.style.display = 'none';
+  boardEl.style.display = 'block';
+  boardEl.classList.add('animated', 'fadeIn', 'slower');
   if (jogador) {
     myCharacter = (jogador === 'luna') ? 'x' : 'o';
     computerCharacter = (myCharacter === 'o') ? 'x' : 'o';
@@ -351,31 +435,27 @@ function start(jogador) {
     ['', '', '']
   ];
   board = [
-    [document.getElementById('cel1'), document.getElementById('cel2'), document.getElementById('cel3')],
-    [document.getElementById('cel4'), document.getElementById('cel5'), document.getElementById('cel6')],
-    [document.getElementById('cel7'), document.getElementById('cel8'), document.getElementById('cel9')]
+    [cel1El, cel2El, cel3El],
+    [cel4El, cel5El, cel6El],
+    [cel7El, cel8El, cel9El]
   ];
   updateBoard();
-  document.getElementById('whoPlay').style.display = 'none';
-  document.getElementById('whoWin').style.display = 'none';
+  whoPlayEl.style.display = 'none';
+  whoWinEl.style.display = 'none';
   if (whoStarts === 0 && myCharacter === 'x' || whoStarts === 1 && myCharacter === 'o') {
     whoStarts = 0;
     whoPlays = whoStarts;
-
-    document.getElementById("whoStarts").style.display = 'block';
-    document.getElementById("whoStarts").innerHTML = "Você inicia!";
-
+    whoStartsEl.style.display = 'block';
+    whoStartsEl.innerHTML = "Você inicia!";
   } else {
     whoStarts = 1;
     whoPlays = whoStarts;
-    
-    document.getElementById("whoStarts").style.display = 'block';
-    document.getElementById("whoStarts").innerHTML = "Computador inicia!";
+    whoStartsEl.style.display = 'block';
+    whoStartsEl.innerHTML = "Computador inicia!";
 
     cpuMoves();
   }
   clearStyle();
-
 }
 
 function clearStyle() {
@@ -387,4 +467,18 @@ function clearStyle() {
 
 }
 
-//window.addEventListener("load", start);
+function playBackAudio() {
+  if (audioOnOff) {
+    playMusic.currentTime = 0;
+    playMusic.loop = true;
+    playMusic.autoplay = true;
+    playMusic.preload = 'none';
+    var playPromise = playMusic.play();
+    if (playPromise !== undefined) {
+      playPromise.then(() => {
+      })
+        .catch(error => {
+        });
+    }
+  }
+}
